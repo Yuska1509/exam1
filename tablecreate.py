ada03fe9b64c98c80c1170d329d446819469c76c
Юлия Смолярова <Yuska1509@gmail.com>
2017-05-20 10:06:55 +0300

    newnames

--- tablecreate.py
+++
@@ -1,8 +0,0 @@
-import sqlite
-a = sqlite.db()
-#a.query("CREATE TABLE Actors(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, name INTEGER NOT NULL,"
-#        " href TEXT NOT NULL, film TEXT NOT NULL);")
-
-def addActor(name, href, film):
-    a.query("INSERT INTO Actors(name, href, film) values (\'%s\', \'%s\', \'%s\');" %(name, href, film))
-    a.save()
